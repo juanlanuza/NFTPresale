4aeff4859f7192d4b82608705ab680a3b28c262c
Juan Manuel Lanuza <juanlanuza@users.noreply.github.com>
2022-05-16 21:53:09 +0200

    Revert "Revert "changes to the contract""
    This reverts commit 5565448b2805d4a48445201e866d84812ce9882d.

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,14 +29,12 @@ module.exports = {
     // testnet: {
     //   url: "https://data-seed-prebsc-1-s1.binance.org:8545",
     //   chainId: 97,
-    //   gasPrice: 20000000000,
     //   accounts: [privateKey]
     // }
     // ,
     // mainnet: {
     //   url: "https://bsc-dataseed.binance.org/",
     //   chainId: 56,
-    //   gasPrice: 20000000000,
     //   accounts: [privateKey]
     // }
   },
